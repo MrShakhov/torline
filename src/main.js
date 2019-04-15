b436471d9d030b7e0f86207f2839792404b932e7
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-04-15 10:28:24 +0300

    Added Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronRight, faChevronLeft, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faStar } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faChevronRight,
+    faChevronLeft,
+    faStar,
+    faSpinner);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
