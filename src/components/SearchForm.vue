ed95da31e955a06f37f1d820abeb21476eaa4d59
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-04-17 13:17:47 +0300

    Created new component - SearchForm

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <form class="search-form">
+        <input type="search" class="search-field">
+        <button-standard class="button-standard">
+            <font-awesome-icon icon="search"></font-awesome-icon>
+        </button-standard>
+    </form>
+    
+</template>
+
+<script>
+    import ButtonStandard from "./ButtonStandard";
+    export default {
+        name: "SearchForm",
+
+        components: {
+            ButtonStandard
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+    @import "../less/variables";
+
+    .search-field {
+        padding: 0.14em;
+
+        font-size: 1.2em;
+
+        border: 2px solid #colors[primary];
+        border-right: none;
+        border-radius: 5px 0 0 5px;
+    }
+
+    .button-standard {
+        padding: 0.14em 0.3em;
+
+        color: #colors[primary];
+        font-size: 1.2em;
+
+        border: 2px solid #colors[primary];
+        border-left: none;
+        border-radius: 0 5px 5px 0;
+    }
+
+</style>
