345704d6d849cd55f2a459713c9bfd7ae9c3ad16
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-04-22 18:40:08 +0300

    Added icon "star" in Font Awesome library

--- src/main.js
+++ src/main.js
@@ -7,17 +7,17 @@ import {
     faChevronRight,
     faChevronLeft,
     faSpinner,
-    faSearch
+    faSearch,
+    faStar
 } from '@fortawesome/free-solid-svg-icons'
-import { faStar } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(
     faChevronRight,
     faChevronLeft,
-    faStar,
     faSpinner,
-    faSearch
+    faSearch,
+    faStar
 );
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
