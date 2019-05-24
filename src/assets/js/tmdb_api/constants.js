5ec71e022950617343f30534ee85da0a19fc2244
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-05-24 19:25:15 +0300

    Added library "tmdb_api"

---
+++ src/assets/js/tmdb_api/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = '0b771070b72e43da48055b81f73de132';
---
+++ src/assets/js/tmdb_api/get_movie_by_id.js
@@ -0,0 +1,26 @@
+import { API_KEY } from './constants';
+
+export function getMovieById( id, { videos, credits } ) {
+
+    return new Promise((resolve, reject) => {
+
+        // Validation of arguments
+        if (typeof id !== 'number') reject( new Error('Invalid argument "id"') );
+
+        // Preparing url
+        let url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=ru`;
+        if (videos || credits) {
+            const append = [];
+            if (videos) append.push('videos');
+            if (credits) append.push('credits');
+            url += `&append_to_response=${append.toString()}`;
+        }
+
+        // Sending request
+        fetch(url)
+            .then( response => response.json() )
+            .then( response => resolve(response) );
+
+    });
+
+}
