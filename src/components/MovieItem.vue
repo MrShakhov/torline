a9c655d3cabaa705fd9183fe967eacb1bac9ad37
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-05-27 18:57:47 +0300

    Tag <h1> replaced by <h3>

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -11,7 +11,7 @@
             </figure>
             <!--poster-->
 
-            <h1 class="title">{{ title }}</h1>
+            <h3 class="title">{{ title }}</h3>
             <!--title-->
 
             <div class="properties">
