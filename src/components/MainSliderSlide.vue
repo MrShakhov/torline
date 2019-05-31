9d2e074dd2dedd40dd9e55bd06abebf21a4f9938
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-05-31 19:17:09 +0300

    Added mixin "movie"

--- src/components/MainSliderSlide.vue
+++ src/components/MainSliderSlide.vue
@@ -1,174 +1,91 @@
 <template>
-    <article class="slide"
-             :style="background"
-    >
+    <article class="main-slider-slide" :style="background">
+
         <figure class="poster animated rotateIn">
-            <img :src="posterUrl"
+            <img v-if="posterFileName !== ''"
+                 :src="posterUrl"
                  :alt="`Постер ${title}`"
-                 @load="checkLoading"
                  class="loadable-main-slider-slide"
+                 @load="checkLoading"
             >
         </figure>
+
         <div class="content">
+
             <header>
-                <div>
-                    <h1 class="animated fadeInDown">{{ title }}</h1>
-                    <span class="original-title animated fadeIn">{{ originalTitle }}</span>
-                </div>
-                <span class="description animated fadeIn">{{ description }}</span>
+                <h3 class="title animated fadeInDown">{{ title }}</h3>
+                <span class="original-title animated fadeIn">{{ originalTitle }}</span>
+                <span class="short-properties animated fadeIn">{{ shortProperties }}</span>
                 <div class="buttons">
-                    <button-lite class="button-lite animated jackInTheBox">Смотреть</button-lite>
-                    <button-lite class="button-lite animated jackInTheBox" :disabled="!trailer.provider">Трейлер</button-lite>
+                    <button-lite class="animated jackInTheBox">Смотреть</button-lite>
+                    <button-lite class="animated jackInTheBox" :disabled="videos.length === 0">Трейлер</button-lite>
                 </div>
             </header>
-            <dl class="properties">
-                <div class="animated fadeInDown">
+
+            <div class="properties">
+                <dl class="column animated fadeInDown">
                     <div class="dt-dd-wrapper">
-                        <dt>Жанры:</dt>
-                        <dd>{{ genres.join(', ') }}</dd>
+                        <dt>Жанры</dt>
+                        <dd>{{ genresStr }}</dd>
                     </div>
                     <div class="dt-dd-wrapper">
-                        <dt>Режиссёр:</dt>
-                        <dd>{{ director }}</dd>
+                        <dt>Режиссёр</dt>
+                        <dd>{{ director.name }}</dd>
                     </div>
-                </div>
-                <div class="animated fadeInRight">
-                    <dt>Актеры:</dt>
-                    <dd>{{ cast.join(', ') }}</dd>
-                </div>
-            </dl>
+                </dl>
+                <dl class="column animated fadeInRight">
+                    <div class="dt-dd-wrapper">
+                        <dt>Актеры</dt>
+                        <dd>{{ castStr }}</dd>
+                    </div>
+                </dl>
+            </div>
+
             <p class="overview animated fadeInUp">
                 {{ overview }}
             </p>
+
         </div>
+        <!--content-->
+
     </article>
 </template>
 
 <script>
+    // Components
     import ButtonLite from './ButtonLite';
+
+    //Mixins
     import loading from '../mixins/loading';
+    import movie from '../mixins/movie';
 
     export default {
         name: 'MainSliderSlide',
 
         mixins: [
-            loading
+            loading,
+            movie
         ],
-
-        props: {
-            tmdbId: {
-                type: Number,
-                required: true
-            }
-        },
 
         components: {
             ButtonLite
         },
 
-        data() {
-            return {
-                title: '',
-                originalTitle: '',
-                year: 0,
-                duration: '',
-                backdropPath: '',
-                posterFileName: '',
-                genres: [],
-                overview: '',
-                countries: [],
-                rating: 0,
-                trailer: {},
-                cast: [],
-                director: '',
-            };
-        },
-
         computed: {
-            description() {
-                return `${this.year} / ${this.duration} / tMDB ${this.rating} / ${this.countries.join(', ')}`;
-            },
             background() {
-                if (!this.backdropPath) return; // Если имя файла еще не пришло с tMDB, то ничего не делаем
+                if (!this.backdropFileName) return;
 
-                let image = `url("https://image.tmdb.org/t/p/w1280${this.backdropPath}") center / cover no-repeat`;
+                let image = `url("https://image.tmdb.org/t/p/w1280${this.backdropFileName}") center / cover no-repeat`;
                 let gradient = 'linear-gradient( rgba(51, 51, 51, 0.7), rgba(51, 51, 51, 0.7) )';
 
                 return {
                     background: `${gradient}, ${image}`
                 }
-            },
-            posterUrl() {
-                // Если имя файла еще не пришло с tMDB, то ничего не делаем
-                if (!this.posterFileName) return;
-
-                const baseUrl = 'https://image.tmdb.org/t/p/w342';
-                return baseUrl + this.posterFileName;
             }
         },
 
-        methods: {
-        },
-
         created() {
-            // Запрашиваем информацию о фильме с tMDB и заполняем поля экземпляра
-            let url = `https://api.themoviedb.org/3/movie/${this.tmdbId}?api_key=0b771070b72e43da48055b81f73de132&language=ru&append_to_response=videos%2Ccredits`;
-            fetch(url)
-                .then( (response) => response.json() )
-                .then( (response) => {
-                    this.originalTitle = response.original_title;
-                    this.overview = response.overview;
-                    this.posterFileName = response.poster_path;
-                    this.year = response.release_date.slice(0, 4);
-                    this.duration = getDurationFromMinutes(response.runtime);
-                    this.title = response.title;
-                    this.rating = response.vote_average;
-                    this.backdropPath = response.backdrop_path;
-
-                    if (response.videos.results.length > 0) {
-                        this.trailer = {
-                            provider: response.videos.results[0].site,
-                            key: response.videos.results[0].key
-                        };
-                    }
-
-                    /*
-                        Отфильтровываем не нужные нам данные из ответа,
-                        нужные записываем в соответстующие свойства
-                    */
-
-                    // Парсим жанры
-                    response.genres.forEach( (item) => {
-                        this.genres.push(item.name);
-                    } );
-
-                    // Парсим страны
-                    response.production_countries.forEach( (item) => {
-                        this.countries.push(item.name);
-                    } );
-
-                    // Парсим актеров
-                    for (let i = 0; i < 10 && i < response.credits.cast.length; i++) {
-                        this.cast.push(response.credits.cast[i].name);
-                    }
-
-                    // Парсим режисера
-                    for (let i = 0; i < response.credits.crew.length; i++) {
-
-                        if (response.credits.crew[i].job === 'Director') {
-                            this.director = response.credits.crew[i].name;
-                            break;
-                        }
-                    }
-
-                    // Functions
-                    function getDurationFromMinutes(mins) {
-                        let hours = Math.floor(mins / 60);
-                        let minutes = mins % 60;
-                        return `${hours} ч ${minutes} мин`;
-                    }
-
-                } );
+            this.loadContent();
         }
     };
 </script>
@@ -178,40 +95,27 @@
 
     @animationDelay: 1.1s;
 
-    .slide {
-        display: flex;
+    .main-slider-slide {
         align-items: center;
-
-        box-sizing: border-box;
-
-        overflow: hidden;
-
-        padding: 3% 5%;
-
-        border-radius: 5px;
-
         box-shadow: 0 0 15px #colors[primary];
-
+        box-sizing: border-box;
+        border-radius: 5px;
         color: #colors[white];
-
         cursor: pointer;
+        display: flex;
+        overflow: hidden;
+        padding: 3% 5%;
     }
 
     .poster {
-        margin: 0 5% 0 0;
-        width: 22%;
-
-        flex-shrink: 0;
-
-        border-radius: 5px;
-
-        box-shadow: 0 0 15px #colors[secondary];
-
-        overflow: hidden;
-
-        transition: box-shadow #times[hover];
-
         animation-delay: @animationDelay;
+        border-radius: 5px;
+        box-shadow: 0 0 15px #colors[secondary];
+        flex-shrink: 0;
+        margin: 0 5% 0 0;
+        overflow: hidden;
+        transition: box-shadow #times[hover];
+        width: 22%;
 
         img {
             display: block;
@@ -220,60 +124,100 @@
     }
 
     .content {
-        position: relative;
         max-height: 100%;
         overflow: hidden;
+        position: relative;
     }
 
-    h1 {
+    .title {
+        animation-delay: @animationDelay + 0.2s;
         display: inline-block;
-        margin: 0 0.25em 0.12em 0;
-
         font-size: 2.3em;
         font-weight: 500;
         line-height: 1;
+        margin: 0 0.25em 0.12em 0;
 
-        animation-delay: @animationDelay + 0.2s;
+        @media (max-width: 800px) {
+            font-size: 3.45vw;
+        }
+
+        @media (max-width: 640px) {
+            font-size: 22px;
+        }
+
+        @media (max-width: 450px) {
+            margin-bottom: 0.3em;
+        }
+
+        @media (max-width: 400px) {
+            margin: 0;
+        }
     }
 
     .original-title {
+        animation-delay: @animationDelay + 0.3s;
         color: #colors[lightgray];
         font-weight: 300;
         white-space: nowrap;
 
-        animation-delay: @animationDelay + 0.3s;
+        @media (max-width: 650px) {
+            display: none;
+        }
     }
 
-    .description {
+    .short-properties {
+        animation-delay: @animationDelay + 0.4s;
         color: #colors[lightgray];
+        display: block;
         font-weight: 300;
 
-        animation-delay: @animationDelay + 0.4s;
+        @media (max-width: 400px) {
+            display: none;
+        }
     }
 
     .buttons {
         margin: 1em 0;
 
-        .button-lite {
-            animation-delay: @animationDelay + 1.5s;
+        @media (max-width: 450px) {
+            display: none;
+        }
+    }
+
+    .button-lite {
+        animation-delay: @animationDelay + 1.5s;
+
+        &:not(:last-child) {
+            margin-right: 0.5em;
         }
 
-        .button-lite:not(:last-child) {
-            margin-right: 0.5em;
+        @media (max-width: 800px) {
+            border-width: 2px;
         }
     }
 
     .properties {
+        color: #colors[lightgray];
         display: flex;
+        font-size: 0.9em;
+        line-height: 1.4;
         margin: 0 0 0.6em 0;
 
-        line-height: 1.4;
-        font-size: 0.9em;
-        color: #colors[lightgray];
+        @media (max-width: 760px) {
+            display: none;
+        }
+
+        dl {
+            margin: 0;
+        }
 
         dt {
             display: inline;
             margin-right: 0.5em;
+
+            &:after {
+                content: ':';
+            }
         }
 
         dd {
@@ -281,18 +225,18 @@
             margin: 0;
         }
 
-        & > div:first-child {
+    }
+
+    .column {
+        &:first-child {
+            animation-delay: @animationDelay + 0.6s;
             flex-basis: 40%;
             margin-right: 1em;
-
-            animation-delay: @animationDelay + 0.6s;
         }
 
-        & > div:last-child {
-            flex-basis: 60%;
-            margin-right: 1em;
-
+        &:last-child {
             animation-delay: @animationDelay + 0.7s;
+            flex-basis: 60%;
         }
     }
 
@@ -301,9 +245,12 @@
     }
 
     .overview {
-        margin-top: 0;
-        line-height: 1.4;
-
         animation-delay: @animationDelay + 0.9s;
+        line-height: 1.4;
+        margin-top: 0;
+
+        @media (max-width: 600px) {
+            display: none;
+        }
     }
 </style>
---
+++ src/mixins/movie.js
@@ -0,0 +1,105 @@
+export default {
+    props: {
+        tmdbId: {
+            type: Number,
+            required: true
+        }
+    },
+
+    data() {
+        return {
+            title: '',
+            originalTitle: '',
+            backdropFileName: '',
+            posterFileName: '',
+            releaseDate: '',
+            runtime: 0,
+            rating: 0,
+            countries: [],
+            genres: [],
+            cast: [],
+            crew: [],
+            videos: [],
+            overview: ''
+        }
+    },
+
+    computed: {
+        shortProperties() {
+            return `${this.year} / ${this.duration} / tMDB ${this.rating} / ${this.countriesStr}`;
+        },
+        castStr() {
+            const castNames = [];
+
+            for (let i = 0; i < 10 && i < this.cast.length; i++) {
+                castNames.push(this.cast[i].name);
+            }
+
+            return castNames.join(', ');
+        },
+        posterUrl() {
+            return (this.posterFileName === null) ?
+                '/img/poster.svg' :
+                `https://image.tmdb.org/t/p/w500${this.posterFileName}`;
+        },
+        year() {
+            return this.releaseDate.slice(0, 4);
+        },
+        duration() {
+            const hours = Math.floor(this.runtime / 60);
+            const minutes = this.runtime % 60;
+
+            return `${hours} ч ${minutes} мин`;
+        },
+        genresStr() {
+            const genreTitles = this.genres.map( (item) => item.name );
+
+            return genreTitles.join(', ');
+        },
+        countriesStr() {
+            const countriesTitles = this.countries.map( (item) => item.name );
+
+            return countriesTitles.join(', ');
+        },
+        director() {
+            for (let i = 0; i < this.crew.length; i++) {
+                if (this.crew[i].job === 'Director') return this.crew[i];
+            }
+
+            return {
+                name: 'информация отсутствует'
+            };
+        }
+    },
+
+    methods: {
+        loadContent() {
+            // Loading movie info from TMDB
+            let url = `https://api.themoviedb.org/3/movie/${this.tmdbId}?api_key=0b771070b72e43da48055b81f73de132&language=ru&append_to_response=videos%2Ccredits`;
+            fetch(url)
+                .then( (response) => response.json() )
+                .then( (response) => {
+                    this.originalTitle = response.original_title;
+                    this.overview = response.overview;
+                    this.posterFileName = response.poster_path;
+                    this.backdropFileName = response.backdrop_path;
+                    this.releaseDate = response.release_date;
+                    this.runtime = response.runtime;
+                    this.title = response.title;
+                    this.rating = response.vote_average;
+                    this.videos = response.videos.results;
+                    this.genres = response.genres;
+                    this.countries = response.production_countries;
+                    this.crew = response.credits.crew;
+                    this.cast = response.credits.cast.map( item => {
+
+                        // Renaming key "profile_path" by "photoFileName"
+                        item.photoFileName = item.profile_path;
+                        delete item.profile_path;
+
+                        return item;
+                    } );
+                } );
+        }
+    }
+}
