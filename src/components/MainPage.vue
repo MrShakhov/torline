5a8bca3137af35a679db92e3b174e73f98775f5e
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-06-03 11:21:34 +0300

    Reformatted code of MainPage

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,8 +7,11 @@
 </template>
 
 <script>
+    // Components
     import MainSlider from "./MainSlider";
     import DiscoverSection from "./DiscoverSection";
+
+    // Mixins
     import loading from "../mixins/loading";
 
     export default {
@@ -32,85 +35,6 @@
         @media (max-width: 1440px) {
             margin-top: 3.7vw;
         }
-    }
-
-    .main-slider {
-        height: 530px;
-        margin-bottom: 3em;
-
-        @media (max-width: 1440px) {
-            height: 36.8vw;
-            margin-bottom: 3.7vw;
-        }
-
-        & /deep/ .button-standard {
-            &:disabled {
-                font-size: 2.5em;
-
-                @media (max-width: 800px) {
-                    font-size: 3.75vw;
-                }
-            }
-
-            @media (max-width: 800px) {
-                font-size: 5.25vw;
-            }
-        }
-
-        & /deep/ .button-lite {
-            @media (max-width: 800px) {
-                border-width: 2px;
-            }
-        }
-
-        & /deep/ h1 {
-            @media (max-width: 800px) {
-                font-size: 3.45vw;
-            }
-
-            @media (max-width: 640px) {
-                font-size: 22px;
-            }
-
-            @media (max-width: 450px) {
-                margin-bottom: 0.3em;
-            }
-
-            @media (max-width: 400px) {
-                margin: 0;
-            }
-        }
-
-        & /deep/ .properties {
-            @media (max-width: 760px) {
-                display: none;
-            }
-        }
-
-        & /deep/ .original-title {
-            @media (max-width: 650px) {
-                display: none;
-            }
-        }
-
-        & /deep/ .overview {
-            @media (max-width: 600px) {
-                display: none;
-            }
-        }
-
-        & /deep/ .buttons {
-            @media (max-width: 450px) {
-                display: none;
-            }
-        }
-
-        & /deep/ .description {
-            @media (max-width: 400px) {
-                display: none;
-            }
-        }
-
     }
 
     .discover-section {
