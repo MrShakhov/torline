5005d9aef6c9afe08f36ef2e82617c0d62b10911
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-06-03 11:42:12 +0300

    Reformatted code of SearchForm and TorlineHeader

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,18 +1,19 @@
 <template>
     <form class="search-form" v-on:submit.prevent="onSubmit">
-        <input type="search"
-               class="search-field"
-               required
-               v-model.trim="searchQuery"
-        >
+
+        <input type="search" class="search-field" v-model.trim="searchQuery" required>
+
         <button-standard class="button-standard">
-            <font-awesome-icon icon="search"></font-awesome-icon>
+            <font-awesome-icon icon="search"/>
         </button-standard>
+
     </form>
 </template>
 
 <script>
+    // Components
     import ButtonStandard from "./ButtonStandard";
+
     export default {
         name: "SearchForm",
 
@@ -39,29 +40,84 @@
 
     .search-form {
         position: relative;
+
+        &:hover .search-field,
+        .search-field:focus {
+            @media (max-width: 700px) {
+                border: 2px solid #colors[primary];
+                padding: 0.14em 1.7em 0.14em 0.14em;
+                width: 33vw;
+            }
+
+            @media (max-width: 600px) {
+                border: 2px solid #colors[primary];
+                padding: 0.14em 1.7em 0.14em 0.14em;
+                width: 40vw;
+            }
+
+            @media (max-width: 400px) {
+                width: 100%;
+            }
+        }
+
+        @media (max-width: 1068px) {
+            font-size: 16px;
+        }
     }
 
     .search-field {
-        padding: 0.14em 1.7em 0.14em 0.14em;
-
-        font-size: 1.2em;
-
         border: 2px solid #colors[primary];
         border-radius: 5px;
+        font-size: 1.2em;
+        padding: 0.14em 1.7em 0.14em 0.14em;
+        transition:
+                border #times[hover],
+                padding #times[hover],
+                width #times[hover];
+
+        @media (max-width: 980px) {
+            width: 25vw;
+        }
+
+        @media (max-width: 700px) {
+            border: none;
+            padding: 0;
+            width: 0;
+        }
+
+        @media (max-width: 500px) {
+            border: 2px solid #colors[primary];
+            padding: 0.14em 1.7em 0.14em 0.14em;
+            width: 40vw;
+        }
+
+        @media (max-width: 400px) {
+            width: 100%;
+        }
     }
 
     .button-standard {
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 100%;
-        padding: 0.14em 0.3em;
-
-        color: #colors[primary];
-        font-size: 1.2em;
-
         border: 2px solid #colors[primary];
         border-left: none;
         border-radius: 0 5px 5px 0;
+        color: #colors[primary];
+        font-size: 1.2em;
+        height: 100%;
+        padding: 0.14em 0.3em;
+        position: absolute;
+        right: 0;
+        top: 0;
+
+        @media (max-width: 700px) {
+            border: 2px solid #colors[primary];
+            border-radius: 5px;
+            height: auto;
+        }
+
+        @media (max-width: 500px) {
+            border-left: none;
+            border-radius: 0 5px 5px 0;
+            height: 100%;
+        }
     }
 </style>
--- src/components/TorlineHeader.vue
+++ src/components/TorlineHeader.vue
@@ -1,17 +1,20 @@
 <template>
     <header class="header">
-        <router-link to="/">
-            <div class="logo-wrapper">
-                <img src="../assets/img/logo.svg" alt="Логотип TorLine">
-                <span>Tor</span><span>Line</span>
-            </div>
+
+        <router-link to="/" class="logo-wrapper">
+            <img src="../assets/img/logo.svg" alt="Логотип TorLine">
+            <span>Tor</span><span>Line</span>
         </router-link>
+
         <span class="tagline">Смотри торрент онлайн</span>
-        <search-form class="search-form"></search-form>
+
+        <search-form class="search-form"/>
+
     </header>
 </template>
 
 <script>
+    // Components
     import SearchForm from "./SearchForm";
 
     export default {
@@ -27,43 +30,47 @@
     @import "../less/variables";
 
     .header {
+        align-items: center;
+        background: linear-gradient(
+                        135deg,
+                            #colors[primary] 12px, #colors[contrast] 12px,
+                            #colors[contrast] 29px, #colors[primary] 29px
+                        ) left bottom / 41px 13px repeat-x;
         display: flex;
         justify-content: space-between;
-        align-items: center;
         padding: 0.83em 0 15px 0;
-
-        background: linear-gradient(
-                135deg,
-                    #colors[primary] 12px, #colors[contrast] 12px,
-                    #colors[contrast] 29px, #colors[primary] 29px
-        ) left bottom / 41px 13px repeat-x;
 
         @media (max-width: 400px) {
             display: block;
-
             text-align: center;
         }
+    }
 
-        .tagline {
-            font-size: 1.22em;
-            text-align: center;
+    .tagline {
+        font-size: 1.22em;
+        text-align: center;
 
-            @media (max-width: 500px) {
-                display: none;
-            }
+        @media (max-width: 500px) {
+            display: none;
         }
     }
 
     .logo-wrapper {
+        display: block;
         flex-shrink: 0;
-
         font-family: 'Arial', sans-serif;
         font-size: 5em;
         font-weight: 700;
         line-height: 1;
         letter-spacing: -0.05em;
 
-        cursor: pointer;
+        @media (max-width: 800px) {
+            font-size: 7.5vw;
+        }
+
+        @media (max-width: 535px) {
+            font-size: 40px;
+        }
 
         &:hover {
 
@@ -77,99 +84,19 @@
         }
 
         img {
-            max-height: 0.85em;
-            margin-right: 0.15em;
-
             filter: drop-shadow(7px -3px 7px #3333334f);
-
+            margin-right: 0.15em;
+            max-height: 0.85em;
             transition: filter #times[hover];
         }
 
         span {
             text-shadow: 7px -3px 7px #3333334f;
-
             transition: text-shadow #times[hover];
-        }
 
-        span:first-of-type {
-            color: #colors[contrast];
-        }
-
-        @media (max-width: 800px) {
-            font-size: 7.5vw;
-        }
-
-        @media (max-width: 535px) {
-            font-size: 40px;
+            &:first-of-type {
+                color: #colors[contrast];
+            }
         }
     }
-
-    .search-form {
-        & /deep/ .search-field {
-            transition:
-                    width #times[hover],
-                    padding #times[hover],
-                    border #times[hover];
-
-            @media (max-width: 980px) {
-                width: 25vw;
-            }
-
-            @media (max-width: 700px) {
-                width: 0;
-                padding: 0;
-                border: none;
-            }
-
-            @media (max-width: 500px) {
-                width: 40vw;
-                padding: 0.14em 1.7em 0.14em 0.14em;
-                border: 2px solid #colors[primary];
-            }
-
-            @media (max-width: 400px) {
-                width: 100%;
-            }
-        }
-
-        &:hover /deep/ .search-field,
-        & /deep/ .search-field:focus {
-            @media (max-width: 700px) {
-                width: 33vw;
-                padding: 0.14em 1.7em 0.14em 0.14em;
-                border: 2px solid #colors[primary];
-            }
-
-            @media (max-width: 600px) {
-                width: 40vw;
-                padding: 0.14em 1.7em 0.14em 0.14em;
-                border: 2px solid #colors[primary];
-            }
-
-            @media (max-width: 400px) {
-                width: 100%;
-            }
-        }
-
-        & /deep/ .button-standard {
-            @media (max-width: 700px) {
-                height: auto;
-
-                border: 2px solid #colors[primary];
-                border-radius: 5px;
-            }
-
-            @media (max-width: 500px) {
-                height: 100%;
-
-                border-left: none;
-                border-radius: 0 5px 5px 0;
-            }
-        }
-
-        @media (max-width: 1068px) {
-            font-size: 16px;
-        }
-    }
-
 </style>
