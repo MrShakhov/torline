69a5186922eedcbbf1774ddc3f9336d1e11451d1
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-06-10 16:58:20 +0300

    Refactored and reformatted code of SearchResultsPage

--- src/components/SearchResultsPage.vue
+++ src/components/SearchResultsPage.vue
@@ -1,28 +1,36 @@
 <template>
     <div class="search-results-page">
-        <nav class="navigation">
-            <button class="button-back">&lt; Назад</button>
-        </nav>
-        <h1 class="title" v-if="totalResults">По запросу &quot;{{ searchQuery }}&quot; найдено {{ totalResults }}:</h1>
-        <h1 class="title" v-else>По запросу &quot;{{ searchQuery }}&quot; ничего не найдено</h1>
-        <ul class="search-results" v-if="searchResults.length > 0">
-            <li class="item"
-                v-for="item in searchResults"
-            >
-                <movie-item v-bind="item"
-                            class="loadable-search-results-page"
-                            @load="checkLoading"
-                />
-            </li>
-        </ul>
-        <div class="preloader" v-if="totalResults > searchResults.length">
+
+        <button class="button-back" @click="$router.back()">&lt; Назад</button>
+
+        <main>
+
+            <h1 class="title" v-if="totalResults">По запросу &quot;{{ searchQuery }}&quot; найдено {{ totalResults }}:</h1>
+            <h1 class="title" v-else>По запросу &quot;{{ searchQuery }}&quot; ничего не найдено</h1>
+
+            <ul class="search-results" v-if="searchResults.length > 0">
+                <li class="item" v-for="item in searchResults">
+                    <movie-item v-bind="item"
+                                class="loadable-search-results-page"
+                                @load="checkLoading(null, removeLoadingFlag, addItemsIfNeeded)"
+                    />
+                </li>
+            </ul>
+
+        </main>
+
+        <div class="loader" v-if="newItemsAreLoading">
             <font-awesome-icon icon="spinner" pulse/>
         </div>
+
     </div>
 </template>
 
 <script>
+    // Components
     import MovieItem from './MovieItem';
+
+    // Mixins
     import loading from '../mixins/loading';
 
     export default {
@@ -59,10 +67,9 @@
 
         methods: {
             loadItems(page = 1) {
-                return new Promise( (resolve, reject) => {
-                    if (!this.searchQuery) reject( new Error('Empty property "searchQuery" of "SearchResultsPage"') );
+                return new Promise( resolve => {
 
-                    // preparing a url
+                    // preparing url
                     const url = `https://api.themoviedb.org/3/search/movie?api_key=0b771070b72e43da48055b81f73de132&language=ru&query=${this.searchQuery}&page=${page}&region=RU`;
 
                     // setting the loading flag
@@ -70,14 +77,19 @@
 
                     // send query
                     fetch(url)
-                        .then( (response) => response.json() )
-                        .then( (response) => {
-                            this.totalResults = response.total_results;
-                            if (!this.totalResults) this.$emit('load');
+                        .then( response => response.json() )
+                        .then( response => {
 
+                            this.totalResults = response.total_results;
+
+                            // if nothing is found emit "load" event to
+                            // hide loading screen and show page
+                            if (!this.totalResults) this.checkLoading();
+
+                            // parsing of search results
                             response.results.forEach( (item) => {
                                 this.searchResults.push({
-                                    tmdbId: +item.id,
+                                    tmdbId: item.id,
                                     posterFileName: item.poster_path,
                                     releaseDate: item.release_date,
                                     title: item.title,
@@ -85,14 +97,14 @@
                                 });
                             } );
 
-                            // remove the loading flag
-                            this.newItemsAreLoading = false;
-
                             resolve();
                         } );
                 } );
             },
-            addItems() {
+            removeLoadingFlag() {
+                this.newItemsAreLoading = false;
+            },
+            addItemsIfNeeded() {
                 // checking the loading flag
                 if (this.newItemsAreLoading) return;
 
@@ -110,10 +122,11 @@
 
         watch: {
             searchQuery() {
-                // reset
+                // resetting
                 this.searchResults = [];
                 this.isLoaded = false;
                 this.loadedSourcesCount = 0;
+                this.newItemsAreLoading = false;
 
                 this.loadItems();
             }
@@ -121,12 +134,11 @@
 
         created() {
             this.loadItems();
-            this.$on('load', this.addItems);
-            window.addEventListener('scroll', this.addItems);
+            window.addEventListener('scroll', this.addItemsIfNeeded);
         },
 
         beforeDestroy() {
-            window.removeEventListener('scroll', this.addItems);
+            window.removeEventListener('scroll', this.addItemsIfNeeded);
         }
     }
 </script>
@@ -138,33 +150,25 @@
         margin-top: 1.5em;
     }
 
-    .navigation {
-        margin-bottom: 1.5em;
+    .button-back {
+        background: none;
+        border: none;
+        cursor: pointer;
+        font-size: 1.22em;
+        font-weight: 100;
+        margin: 0 0 1em 1em;
+        padding: 0;
+        transition: color #times[hover];
 
-        .button-back {
-            margin-left: 1em;
-            padding: 0;
-
-            border: none;
-
-            background: none;
-
-            cursor: pointer;
-
-            font-size: 1.22em;
-            font-weight: 100;
-
-            transition: color #times[hover];
-
-            &:hover {
-                color: #colors[contrast];
-            }
+        &:hover {
+            color: #colors[contrast];
         }
     }
 
     .title {
-        font-weight: 300;
         font-size: 1.2em;
+        font-weight: 300;
+        margin: 0 0 0.7em 0;
     }
 
     .search-results {
@@ -177,10 +181,9 @@
 
     .item {
         flex-shrink: 0;
-        width: 189px;
-        margin-bottom: 1.5em;
-
         list-style: none;
+        margin-bottom: 1.5em;
+        width: 189px;
 
         @media (max-width: 1200px) {
             width: 15.75vw;
@@ -200,10 +203,9 @@
         margin-right: 15%;
     }
 
-    .preloader {
-        margin: 1em 0;
-
-        text-align: center;
+    .loader {
         font-size: 3em;
+        margin: 1em 0;
+        text-align: center;
     }
 </style>
