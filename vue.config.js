1b339c9fe6b0eafff5c7d099a92325889deca900
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-06-11 16:46:19 +0300

    Added vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pwa: {
+        name: 'TorLine',
+        themeColor: '#ffffff',
+        msTileColor: '#00aba9',
+        iconPaths: {
+            appleTouchIcon: 'img/icons/apple-touch-icon.png'
+        }
+    }
+};
