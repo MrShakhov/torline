94e9d44a1d011935fd6b0e976859d6e03dfa1626
Alfred Shakhov <a.e.shakhov@gmail.com>
2019-06-13 18:48:42 +0300

    Added vuex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import store from './store';
+import './registerServiceWorker';
 
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -11,7 +13,6 @@ import {
     faSearch,
     faStar
 } from '@fortawesome/free-solid-svg-icons';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 library.add(
     faChevronRight,
@@ -21,13 +22,12 @@ library.add(
     faStar
 );
 Vue.component('font-awesome-icon', FontAwesomeIcon);
-
-// Service Worker
-import './registerServiceWorker';
+// End Font Awesome
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
+    store,
     render: h => h(App)
 }).$mount('#app');
